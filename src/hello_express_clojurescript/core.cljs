2136f3970fdf267100f0e71d2572214f27f32805
Frank Hale <frankhale@gmail.com>
2015-09-28 18:46:07 -0400

    Make code look better

--- src/hello_express_clojurescript/core.cljs
+++ src/hello_express_clojurescript/core.cljs
@@ -10,47 +10,7 @@
 (def cookieParser (node/require "cookie-parser"))
 (def bodyParser (node/require "body-parser"))
 (def app (express))
-
-; routes.js and hello-express.js are in the root of my project. These will be
-; moved to a build dir once I figure out how to properly deploy an Express app
-; using Clojurescript. For now this is all new ground, playing and learning so
-; as long as it works it's all good!
-
-(def routes (node/require "./routes")) ; is this going to work?
-
-;;; Will add these guys later
-
-(.set app "views" (.join path js/__dirname "views"))
-(.set app "view engine" "hbs")
-
-(.use app (logger "dev"))
-(.use app (.json bodyParser))
-(.use app (.urlencoded bodyParser #js { :extended false }))
-(.use app (.static express (.join path js/__dirname "public")))
-
-(.use app "/" routes)
-
-; (.get app "/" (fn [req res next]
-;   (.render res "index" #js { :title "Express from Clojurescript" })))
-;
-; (.get app "/users" (fn [req res]
-;   (.send res "respond with a resource")))
-
-(.use app (fn [req res next]
-  (let [err (js/Error. "Not Found")]
-    (set! (.-status err) 404)
-    (next err))))
-
-(defn setup-error-handler []
-  (.use app (fn [err req res next]
-    (let [dev? (if (= (.get app "env") "development") true false)
-          err-status (.-status err)
-          err-message (.-message err)]
-      (.status res (if-not (nil? err-status) err-status 500))
-      (.render res "error" #js {
-        :message err-message
-        :error (if dev? err #js {})})))))
-
+(def routes (node/require "./routes"))
 (def http (node/require "http"))
 (def server (.createServer http app))
 (def debug ((node/require "debug") "hello-express:server"))
@@ -66,7 +26,29 @@
 (def port (normalizePort
   (if-not (nil? (.-env.PORT node/process)) (.-env.PORT node/process) "3000")))
 
-(.set app "port" port)
+(defn setup-express-config []
+  (.set app "port" port)
+  (.set app "views" (.join path js/__dirname "views"))
+  (.set app "view engine" "hbs")
+  (.use app (logger "dev"))
+  (.use app (.json bodyParser))
+  (.use app (.urlencoded bodyParser #js { :extended false }))
+  (.use app (.static express (.join path js/__dirname "public")))
+  (.use app "/" routes)
+  (.use app (fn [req res next]
+    (let [err (js/Error. "Not Found")]
+      (set! (.-status err) 404)
+      (next err)))))
+
+(defn setup-error-handler []
+  (.use app (fn [err req res next]
+    (let [dev? (if (= (.get app "env") "development") true false)
+          err-status (.-status err)
+          err-message (.-message err)]
+      (.status res (if-not (nil? err-status) err-status 500))
+      (.render res "error" #js {
+        :message err-message
+        :error (if dev? err #js {})})))))
 
 (defn server-error-handler [error]
   (if-not (= (.-syscall error) "listen")
@@ -84,10 +66,9 @@
         bind (if (= (type addr) "string") (str "pipe" addr) (str "port" (.-port addr)))]
     (debug (str "Listening on " bind))))
 
-
 (defn -main [& args]
-  ; This will be replaced later
-  (println "Starting Express...")
+  (println "Starting up Express...")
+  (setup-express-config)
   (setup-error-handler)
   (.listen server port)
   (.on server "error" server-error-handler)
